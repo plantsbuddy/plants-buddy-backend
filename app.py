4b4ced7e97416bc025ee29c29f0e3acb4e5d8e4b
hazzam <hazzamapps@gmail.com>
2023-03-29 19:52:29 +0500

    first commit

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, request, jsonify
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    # A welcome message to test our server
+    return "<h1>Welcome to our medium-greeting-api!</h1>"
+
+
+if __name__ == '__main__':
+    # Threaded option to enable multiple instances for multiple user access support
+    app.run(threaded=True, port=5000)
