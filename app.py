f35c1dc02f601c4bc3afd4127e0af4a793c615f6
hazzam <plantsbuddyfyp@gmail.com>
2023-03-29 22:36:49 +0500

    first commit

--- app.py
+++ app.py
@@ -8,6 +8,12 @@ def index():
     return "<h1>Welcome to our Mibalo!</h1>"
 
 
+@app.route('/students')
+def index():
+    # A welcome message to test our server
+    return "<h1>Here are some students!</h1>"
+
+
 # if __name__ == '__main__':
 #     # Threaded option to enable multiple instances for multiple user access support
 #     app.run(port=process.env.PORT)
